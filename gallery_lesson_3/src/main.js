95fa2ce6c6480f9cb03f8ad67b1967064fb8b46c
JuliaKyz <Julie.abukhova@gmail.com>
2022-09-29 18:48:44 +0300

    added lesson 3

---
+++ gallery_lesson_3/src/main.js
@@ -0,0 +1,8 @@
+
+import { drawGalleryItem } from "./item";
+import items from "./items";
+
+const galleryRootElement = document.querySelector(".gallery");
+console.log(galleryRootElement);
+
+items.map(item => galleryRootElement.appendChild(drawGalleryItem(item)));
